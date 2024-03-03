642e49913038d67a48ce77a0437ec75bb437f912
TadashiM <mahi@sf6.so-net.ne.jp>
2024-03-03 16:49:27 +0900

    Initial Commit

---
+++ A2024030301/A2024030301App.swift
@@ -0,0 +1,17 @@
+//
+//  A2024030301App.swift
+//  A2024030301
+//
+//  Created by Tadashi Matsuhiro on 2024/03/03.
+//
+
+import SwiftUI
+
+@main
+struct A2024030301App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A2024030301/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A2024030301
+//
+//  Created by Tadashi Matsuhiro on 2024/03/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
